f2416962a74843bb9b0fd9f924662f3ce73bff7b
sieunnnn <wldsmtldsm65@gmail.com>
2023-10-01 02:55:12 +0900

    Rename: Optional 이해 잘못해서 이상하게 작성한 코드 간결하게 수정

--- orm-jpa/src/main/java/wanted/pre_assignment/service/ApplyService.java
+++ orm-jpa/src/main/java/wanted/pre_assignment/service/ApplyService.java
@@ -23,10 +23,8 @@ public class ApplyService {
     @Transactional
     public void apply(ApplyRequest request) {
         // 유저 존재 여부 확인
-        Optional<User> user = userRepository.findUserById(request.getUserId());
-        if (user.isEmpty()) {
-                throw  new EntityNotFoundException("찾는 유저가 존재하지 않습니다. 유저 아이디 :  " + request.getUserId());
-        }
+        User user = userRepository.findUserById(request.getUserId())
+                .orElseThrow(() -> new EntityNotFoundException("찾는 유저가 존재하지 않습니다. 유저 아이디 :  " + request.getUserId()));
 
         // 이미 지원한 유저 인지 확인
         JobPosting jobPosting = jobPostingRepository.findJobPostingById(request.getJobPostingId())
@@ -34,11 +32,11 @@ public class ApplyService {
 
         List<User> users = jobPosting.getUsers();
         for (User applyUser : users) {
-            if (user.get().equals(applyUser.getId())) {
+            if (user.equals(applyUser.getId())) {
                 throw new IllegalStateException("이미 지원한 공고입니다.");
             }
         }
 
-        user.get().updateUserJobPosting(jobPosting);
+        user.updateUserJobPosting(jobPosting);
     }
 }
